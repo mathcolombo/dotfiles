be740f8e2f62560d22dde374078dc5c3bbe98ebd
Matheus Colombo <matheus.colombo21@gmail.com>
2026-09-04 00:33:33 -0300

    feat: add wpps and remove windows borders

--- dot_config/hypr/modules/ui/general.lua
+++ dot_config/hypr/modules/ui/general.lua
@@ -9,12 +9,12 @@ hl.config({
         gaps_in  = 5,
         gaps_out = 20,
 
-        border_size = 2,
+        -- border_size = 1,
 
-        col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-            inactive_border = "rgba(595959aa)",
-        },
+        -- col = {
+        --     active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+        --     inactive_border = "rgba(595959aa)",
+        -- },
 
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
         resize_on_border = false,
